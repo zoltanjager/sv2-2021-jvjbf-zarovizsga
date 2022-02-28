5c27de9b82845ba44c1edf108bd53b2bca4cf351
zoltanjager <79867348+zoltanjager@users.noreply.github.com>
2022-02-28 12:33:51 +0100

    Refactor videos

--- sv2-2021-jvjbf-zarovizsga/src/main/java/videos/VideoPlatform.java
+++ sv2-2021-jvjbf-zarovizsga/src/main/java/videos/VideoPlatform.java
@@ -5,15 +5,15 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
-
 import java.util.List;
-
 
 public class VideoPlatform {
     private List<Channel> channels = new ArrayList<>();
 
     public int calculateSumOfVideos() {
-        return channels.stream().mapToInt(Channel::getNumberOfVideos).sum();
+        return channels.stream()
+                .mapToInt(Channel::getNumberOfVideos)
+                .sum();
     }
 
     public void readDataFromFile(Path path) {
@@ -25,19 +25,24 @@ public class VideoPlatform {
         }
     }
 
+    public List<Channel> getChannels() {
+        return channels;
+    }
+
     private void readLines(BufferedReader reader) throws IOException {
         String line;
         reader.readLine();
         while ((line = reader.readLine()) != null) {
-            String[] parts = line.split(";");
-            String channelName = parts[0];
-            int subscriptions = Integer.parseInt(parts[1]);
-            int numberOfVideos = Integer.parseInt(parts[2]);
-            channels.add(new Channel(channelName, subscriptions, numberOfVideos));
+            createNewChannelObj(line);
         }
     }
 
-    public List<Channel> getChannels() {
-        return channels;
+    private void createNewChannelObj(String line) {
+        String[] parts = line.split(";");
+        String channelName = parts[0];
+        int subscriptions = Integer.parseInt(parts[1]);
+        int numberOfVideos = Integer.parseInt(parts[2]);
+        channels.add(new Channel(channelName, subscriptions, numberOfVideos));
     }
+
 }
